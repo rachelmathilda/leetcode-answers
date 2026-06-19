023edb0ddf6f60a6cf4b53de618e505795ce0493
rachelmathilda <rachelmathilda10@gmail.com>
2026-06-19 10:25:19 +0700

    feat: add stickers_to_spell_word unsolved implementation

--- stickers_to_spell_word.py
+++ stickers_to_spell_word.py
@@ -1,4 +1,32 @@
 # exhaustive search backtracking 
 
+def can_complete(stickers, word):
+    for char in word:
+        if not any(char in sticker for sticker in stickers):
+            return False
+    return True
+
 def stickers_to_spell_word(stickers, word):
-    print(stickers, word)
+    if can_complete(stickers, word):
+        min_stickers = 0 
+        # exhaustive search
+        for sticker in stickers:
+            for char in sticker: 
+                if char in word:
+                    # make choice
+                    word = word.replace(char, "", 1)
+                    # backtrack
+                    if min_stickers < 1 + stickers_to_spell_word(stickers, word):
+                        # undo choice
+                        word = word + char
+                        return min_stickers
+                    else: 
+                        return 1 + stickers_to_spell_word(stickers, word)
+                else:
+                    continue
+    else: 
+        return 0
+    return min_stickers
+                
+print(stickers_to_spell_word(["with", "example", "science"], "thehat"))
+print(stickers_to_spell_word(["notice", "possible"], "basicbasic"))
